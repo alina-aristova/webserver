39986c8b7d8e24f01a608e4f40d22f749a28ce76
pobortsevv <pobortcevv@gmail.com>
2021-06-29 18:33:35 +0300

    Добавил большую наглядность использования configuration класса

--- config_parse/srcs/main.cpp
+++ config_parse/srcs/main.cpp
@@ -2,15 +2,18 @@
 
 int main(void)
 {
+	std::vector<Server> servers;
+
 	try
 	{
 		Configuration test("default.conf");
-		
+		servers = test.getServers();	
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
-	
+	if (servers.size() == 0)
+		return 1;
 	return 0;
 }
